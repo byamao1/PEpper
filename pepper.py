5c8e7b2c267d7e990b85664831fe0e90b32a872c
baiyu <305230444@qq.com>
2020-08-06 09:38:58 +0800

    Add time counter

--- pepper.py
+++ pepper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import time
 
 from modules import banner
 from modules import argv
@@ -8,7 +9,7 @@ from modules import output
 import sys
 
 
-def main(csv_name:str='output.csv'):
+def start_task(csv_name: str = 'output.csv'):
     argv.get()
     banner.get()
     csv = open(csv_name, 'w')
@@ -17,19 +18,27 @@ def main(csv_name:str='output.csv'):
               "n_susp_strings,is_packed,"
               "has_certificate,"
               "susp_virustotal_ratio,n_yara_rules")
-    run.get(sys.argv[1], csv)
-    csv.close()
+    try:
+        run.get(sys.argv[1], csv)
+    except Exception as e:
+        print("Throw: ", str(e))
+    finally:
+        csv.close()
     output.get()
 
 
+def main(dir_name_list: list):
+    for i, dir_name in enumerate(dir_name_list):
+        start_time = time.perf_counter()
+        if i == 0 and len(sys.argv) < 2:
+            sys.argv.append(f"./samples/{dir_name}")
+
+        start_task(csv_name=f'{dir_name}.csv')
+
+        # 记录运行整体耗时
+        end_time = time.perf_counter()
+        print(f'Counting [{dir_name}] elapsed {(end_time - start_time) // 60}m {(end_time - start_time) % 60:.2f}s')
+
+
 if __name__ == "__main__":
-    sample_dir_name = 'black'
-    if len(sys.argv) < 2:
-        sys.argv.append(f"./samples/{sample_dir_name}")
-
-    main(csv_name=f'{sample_dir_name}.csv')
-
-    # second
-    sample_dir_name = 'white'
-    sys.argv[1] = f"./samples/{sample_dir_name}"
-    main(csv_name=f'{sample_dir_name}.csv')
+    main(['data', 'data_'])
